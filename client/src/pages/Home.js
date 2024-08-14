982ec9a5a40c7f4efcbf611bb0eeb587bda9a845
Balalomba Barandao <barand14@gmail.com>
2024-08-14 12:38:55 -0400

    proxy issues fixed

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,11 +9,11 @@ const Home = () => {
         <h1>Welcome to RecipeCraft</h1>
         <p>Explore and create amazing recipes. Discover new flavors and share your culinary masterpieces with the world!</p>
         <footer>
-        <p class="follow">Follow Us <a href="https://github.com/drsaho"><i class="li-github"></i> Amadou</a>
-       <a href="https://github.com/barand14"><i class="li-github"></i> Balalomba</a>
-       <a href="https://github.com/EnduringTimes"><i class="li-github"></i> Menes</a></p>
+        <p className="follow">Follow Us <a href="https://github.com/drsaho"><i className="li-github"></i> Amadou</a>
+       <a href="https://github.com/barand14"><i className="li-github"></i> Balalomba</a>
+       <a href="https://github.com/EnduringTimes"><i className="li-github"></i> Menes</a></p>
    
-       <p class="follow">  &copy; 2024 RecipeCraft. All rights reserved.</p>
+       <p className="follow">  &copy; 2024 RecipeCraft. All rights reserved.</p>
         </footer>
         </div>
       </div>
